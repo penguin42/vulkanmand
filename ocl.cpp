8741dde9d55eb58b51e1060e62c52fee918aa9d6
Dr David Alan Gilbert <dave@treblig.org>
2017-11-08 01:49:44 +0000

    Start ray tracer code
    Start code, wire in compilation of kernel

--- ocl.cpp
+++ ocl.cpp
@@ -26,39 +26,59 @@
 
 #define SIZE 256
 
+static void dump_voxels(cl_uchar *mapped) {
+    /* Just dump our buffer */
+    for(int z=0;z<SIZE;z++) {
+      std::cerr << __func__ << std::endl << "Z: " << z << std::endl;
+      for(int y=0;y<SIZE;y++) {
+        for(int x=0;x<SIZE;x++) {
+          std::cerr << std::setw(2) << (int)mapped[z*SIZE*SIZE+y*SIZE+x];
+        }
+        std::cerr << std::endl;
+      }
+    }
+}
+
 static int got_dev(cl::Platform &plat, std::vector<cl::Device> &devices, cl::Device &dev, cl::Context &con)
 {
   std::cout << dev.getInfo<CL_DEVICE_NAME>() << std::endl;
   cl_int err = CL_SUCCESS;
-  cl_uchar* mapped;
+  cl_uchar* mapped_voxels;
 
   try {
-    std::ifstream kernstream("mandel.ocl");
-    std::string kern_str((std::istreambuf_iterator<char>(kernstream)), std::istreambuf_iterator<char>());
     std::vector<std::string> programStrings;
-    programStrings.push_back(kern_str);
+    std::ifstream mandkernstrm("mandel.ocl");
+    std::string mandkstr((std::istreambuf_iterator<char>(mandkernstrm)), std::istreambuf_iterator<char>());
+    programStrings.push_back(mandkstr);
+    std::ifstream raykernstrm("ray.ocl");
+    std::string raykstr((std::istreambuf_iterator<char>(raykernstrm)), std::istreambuf_iterator<char>());
+    programStrings.push_back(raykstr);
     cl::Program prog(con, programStrings);
 
+    cl_int buildErr = CL_SUCCESS;
     try {
       prog.build(devices);
     }
     catch (...) {
-      cl_int buildErr = CL_SUCCESS;
       auto buildInfo = prog.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&buildErr);
       for (auto &pair : buildInfo) {
         std::cerr << "build gave " << pair.second << std::endl;
       }
       return err;
     }
+    auto buildInfo = prog.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&buildErr);
+    for (auto &pair : buildInfo) {
+      std::cerr << "build gave " << pair.second << std::endl;
+    }
 
-    cl::Kernel kern(prog, "hello", &err);
-    cl::Buffer output(con, CL_MEM_WRITE_ONLY,  SIZE * SIZE * SIZE * sizeof(cl_uchar));
-    kern.setArg(0, output);
+    cl::Kernel mandkern(prog, "mandel", &err);
+    cl::Buffer voxels(con, CL_MEM_WRITE_ONLY,  SIZE * SIZE * SIZE * sizeof(cl_uchar));
+    mandkern.setArg(0, voxels);
     cl::CommandQueue queue(con, dev);
     cl::Event event;
     std::vector<cl::Event> events;
     err = queue.enqueueNDRangeKernel(
-        kern,
+        mandkern,
         cl::NullRange, /* Offsets */
         cl::NDRange(SIZE,SIZE,SIZE), /* Global range */
         cl::NullRange, /* Local range */
@@ -70,7 +90,7 @@ static int got_dev(cl::Platform &plat, std::vector<cl::Device> &devices, cl::Dev
     events.push_back(event);
     cl::Event eventMap;
     queue.enqueueBarrierWithWaitList(&events);
-    mapped = (cl_uchar*)queue.enqueueMapBuffer(output, CL_TRUE /* blocking */, CL_MAP_READ,
+    mapped_voxels = (cl_uchar*)queue.enqueueMapBuffer(voxels, CL_TRUE /* blocking */, CL_MAP_READ,
                            0 /* offset */, 
                            SIZE * SIZE * SIZE * sizeof(cl_uchar) /* size */,
                            &events,
@@ -82,16 +102,7 @@ static int got_dev(cl::Platform &plat, std::vector<cl::Device> &devices, cl::Dev
     eventMap.wait();
     eventBarrier2.wait();
 
-    /* Just dump our buffer */
-    for(int z=0;z<SIZE;z++) {
-      std::cerr << __func__ << std::endl << "Z: " << z << std::endl;
-      for(int y=0;y<SIZE;y++) {
-        for(int x=0;x<SIZE;x++) {
-          std::cerr << std::setw(2) << (int)mapped[z*SIZE*SIZE+y*SIZE+x];
-        }
-        std::cerr << std::endl;
-      }
-    }
+    dump_voxels(mapped_voxels);
   }
   catch (...) {
     std::cerr << __func__ << ": Error: " << err << std::endl;
